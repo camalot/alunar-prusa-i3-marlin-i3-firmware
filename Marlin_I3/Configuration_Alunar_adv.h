10f4b1d80412c98f7ffa5079f642bfcce215ef0b
camalot <camalot@gmail.com>
2017-05-13 16:02:04 -0500

    update the temp for runaway

--- Marlin_I3/Configuration_Alunar_adv.h
+++ Marlin_I3/Configuration_Alunar_adv.h
@@ -17,7 +17,7 @@
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
   #define THERMAL_PROTECTION_PERIOD 60        // Seconds
-  #define THERMAL_PROTECTION_HYSTERESIS 6     // Degrees Celsius
+  #define THERMAL_PROTECTION_HYSTERESIS 10     // Degrees Celsius
 
   /**
    * Whenever an M104 or M109 increases the target temperature the firmware will wait for the
