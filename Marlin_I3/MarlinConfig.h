bfec5460d054b118e93d07163389d7d2bd72edf5
Ryan Conrad <camalot@gmail.com>
2017-12-25 15:58:34 -0600

    merge 1.1.8

--- Marlin_I3/MarlinConfig.h
+++ Marlin_I3/MarlinConfig.h
@@ -15,8 +15,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy 
- * of the GNU General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
@@ -31,6 +30,7 @@
 #include "Configuration.h"
 #include "Configuration_Alunar.h"
 #include "Conditionals_LCD.h"
+#include "tmc_macros.h"
 #include "Configuration_adv.h"
 #include "Configuration_Alunar_adv.h"
 #include "pins.h"
--- Marlin_I3/Marlin_I3.ino
+++ Marlin_I3/Marlin_I3.ino
@@ -31,42 +31,42 @@
 #include "MarlinConfig.h"
 
 #if ENABLED(ULTRA_LCD)
-	#if ENABLED(LCD_I2C_TYPE_PCF8575)
-		#include <Wire.h>
-		#include <LiquidCrystal_I2C.h>
-	#elif ENABLED(LCD_I2C_TYPE_MCP23017) || ENABLED(LCD_I2C_TYPE_MCP23008)
-		#include <Wire.h>
-		#include <LiquidTWI2.h>
-	#elif ENABLED(LCM1602)
-		#include <Wire.h>
-		#include <LCD.h>
-		#include <LiquidCrystal_I2C.h>
-	#elif ENABLED(DOGLCD)
-		#include <U8glib.h> // library for graphics LCD by Oli Kraus (https://github.com/olikraus/U8glib_Arduino)
-	#else
-		#include <LiquidCrystal.h> // library for character LCD
-	#endif
+  #if ENABLED(LCD_I2C_TYPE_PCF8575)
+    #include <Wire.h>
+    #include <LiquidCrystal_I2C.h>
+  #elif ENABLED(LCD_I2C_TYPE_MCP23017) || ENABLED(LCD_I2C_TYPE_MCP23008)
+    #include <Wire.h>
+    #include <LiquidTWI2.h>
+  #elif ENABLED(LCM1602)
+    #include <Wire.h>
+    #include <LCD.h>
+    #include <LiquidCrystal_I2C.h>
+  #elif ENABLED(DOGLCD)
+    #include <U8glib.h> // library for graphics LCD by Oli Kraus (https://github.com/olikraus/U8glib_Arduino)
+  #else
+    #include <LiquidCrystal.h> // library for character LCD
+  #endif
 #endif
 
 #if HAS_DIGIPOTSS
-	#include <SPI.h>
+  #include <SPI.h>
 #endif
 
 #if ENABLED(DIGIPOT_I2C)
-	#include <Wire.h>
+  #include <Wire.h>
 #endif
 
 #if ENABLED(HAVE_TMCDRIVER)
-	#include <SPI.h>
-	#include <TMC26XStepper.h>
+  #include <SPI.h>
+  #include <TMC26XStepper.h>
 #endif
 
 #if ENABLED(HAVE_TMC2130)
-	#include <SPI.h>
-	#include <TMC2130Stepper.h>
+  #include <SPI.h>
+  #include <TMC2130Stepper.h>
 #endif
 
 #if ENABLED(HAVE_L6470DRIVER)
-	#include <SPI.h>
-	#include <L6470.h>
+  #include <SPI.h>
+  #include <L6470.h>
 #endif
