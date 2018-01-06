218207f3314c39fac191e445e02bcbacfc545636
Ryan Conrad <camalot@gmail.com>
2018-01-05 23:32:35 -0600

    less verbosity... out the config

--- .deploy/test.sh
+++ .deploy/test.sh
@@ -17,6 +17,8 @@ done
 [[ -z "${opt_hex// }" ]] && (>&2 echo "missing file to flash") && exit 1;
 [[ ! -f "${opt_hex}" ]] && (>&2 echo "hex file (${opt_hex}) does not exist.") && exit 1;
 
-avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -U flash:w:${opt_hex} -v -v -v -v
+cat /usr/local/etc/avrdude.conf
 
-avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -U flash:v:${opt_hex} -v -v -v -v
+avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -U flash:w:${opt_hex}
+
+avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -U flash:v:${opt_hex}
