f74847b9b683c371c6ec02b7f82a32638ce76d36
Ryan Conrad <camalot@gmail.com>
2018-01-05 23:59:16 -0600

    no erase

--- .deploy/test.sh
+++ .deploy/test.sh
@@ -17,6 +17,6 @@ done
 [[ -z "${opt_hex// }" ]] && (>&2 echo "missing file to flash") && exit 1;
 [[ ! -f "${opt_hex}" ]] && (>&2 echo "hex file (${opt_hex}) does not exist.") && exit 1;
 
-avrdude -p m2560 -c avrispmkII -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -U flash:w:${opt_hex}
+avrdude -p m2560 -c avrispmkII -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -D -U flash:w:${opt_hex}:i
 
-# avrdude -p m2560 -c avrispmkII -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -D -U flash:v:${opt_hex}
+# avrdude -p m2560 -c avrispmkII -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -D -U flash:v:${opt_hex}:i
