e0f445bdc5937a5303d9490cb2ade9137313d4b6
Ryan Conrad <camalot@gmail.com>
2018-01-05 23:02:48 -0600

    change path to conf

--- .deploy/test.sh
+++ .deploy/test.sh
@@ -17,6 +17,6 @@ done
 [[ -z "${opt_hex// }" ]] && (>&2 echo "missing file to flash") && exit 1;
 [[ ! -f "${opt_hex}" ]] && (>&2 echo "hex file (${opt_hex}) does not exist.") && exit 1;
 
-avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/bin/avrdude.conf -U flash:w:${opt_hex} -v -v -v -v
+avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -U flash:w:${opt_hex} -v -v -v -v
 
-avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/bin/avrdude.conf -U flash:v:${opt_hex} -v -v -v -v
+avrdude -p m2560 -c avrispmkII -n -P /dev/ttyACM0 -C /usr/local/etc/avrdude.conf -U flash:v:${opt_hex} -v -v -v -v
