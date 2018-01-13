20dd168cfb8030326c557403bf253752155de99f
Ryan Conrad <camalot@gmail.com>
2018-01-13 00:48:44 -0600

    map pts

--- .deploy/package.sh
+++ .deploy/package.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e;
+
+exit 9;
 cd "${WORKSPACE}/dist" || exit 9;
 zip -r ${CI_PROJECT_NAME}-${CI_BUILD_VERSION}.zip ${CI_PROJECT_NAME}-*.hex;
 cd "${WORKSPACE}" || exit 9;
--- .deploy/test.sh
+++ .deploy/test.sh
@@ -9,8 +9,15 @@ __error() {
 	exit 1;
 }
 
+__warning() {
+	YELLOW='\033[0;33m';
+	NC='\033[0m';
+	DT=$(date '+%F %T');
+	(>&2 echo -e "${YELLOW}[$DT]\t$(basename $0)\t${1:-"UNKNOWN ERROR"}${NC}");
+}
+
 kill_mega() {
-	docker kill "$1" && docker rm "$1";
+	docker kill "$1" ||  docker rm "$1";
 }
 
 for i in "$@"; do
@@ -36,7 +43,7 @@ mkdir -p /tmp/simduino;
 
 uart=$(docker run -d \
 	--user 0 \
-	-v /tmp/simduino:/tmp \
+	-v /tmp/simduino:/dev/pts \
 	-t "${PULL_REPOSITORY}/camalot/mega2560simulator:latest");
 	# --device=/dev/ttyACM0:/dev/pts/0 \
 
@@ -46,29 +53,26 @@ wait_count=0;
 echo "";
 echo "Waiting for device to become ready.";
 while [ $wait_count -lt 60 ]; do
-	[ -e /tmp/simduino/simavr-uart0 ] && wait_count=99;
+	[ -e /tmp/simduino/0 ] && wait_count=99;
 	sleep 1s;
 	wait_count=$[$wait_count+1];
 	echo -n '.';
 done
 echo "";
 
-[ $wait_count -ne 99 ] && kill_mega "$uart" && __error "Timeout exceeded waiting for device to become ready.";
+[ $wait_count -ne 99 ] && kill_mega "$uart" && __warning "Timeout exceeded waiting for device to become ready." && exit 255;
 
-if [[ ! -L /tmp/simduino/simavr-uart0 ]]; then
-	YELLOW='\033[0;33m';
-	NC='\033[0m';
-	DT=$(date '+%F %T');
-	(>&2 echo -e "${YELLOW}[$DT]\t$(basename $0)\tDevice /tmp/simduino/simavr-uart0 not found. Skipping avrdude tests.${NC}");
+if [[ ! -L /tmp/simduino/0 ]]; then
 	kill_mega "$uart";
+	__warning "Device /tmp/simduino/0 not found. Skipping avrdude tests.";
 	exit 255;
 fi
 
 
-avrdude -p m2560 -c arduino -P /tmp/simduino/simavr-uart0 -C /usr/local/etc/avrdude.conf -D -U "flash:w:${opt_hex}:i"
-# avrdude -p m2560 -c avrispmkII -P /tmp/simduino/simavr-uart0 -C /usr/local/etc/avrdude.conf -D -U flash:v:${opt_hex}:i
+avrdude -p m2560 -c arduino -P /tmp/simduino/0 -C /usr/local/etc/avrdude.conf -D -U "flash:w:${opt_hex}:i"
+# avrdude -p m2560 -c avrispmkII -P /tmp/simduino/0 -C /usr/local/etc/avrdude.conf -D -U flash:v:${opt_hex}:i
 
-marlin_data=$(python /bin/marlin-identify.py -d '/tmp/simduino/simavr-uart0' -s 250000);
+marlin_data=$(python /bin/marlin-identify.py -d '/tmp/simduino/0' -s 250000);
 
 [[ ! $marlin_data =~ echo:Marlin ]] && __error "Unable to located Marlin version";
 [[ ! $marlin_data =~ FIRMWARE_NAME:Marlin ]] && __error "Unable to located Marlin FIRMWARE_NAME";
